a8bc46e1780dc1de00bcb63331a170d5aa205eb0
Zhicheng Xu <xzhch1622@gmail.com>
2015-12-02 14:37:50 -0500

    revise run.sh and configurable port

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-node ./bin/www
+echo listening on port $1
+node ./bin/www $1
--- utils/mongo.js
+++ utils/mongo.js
@@ -6,7 +6,7 @@ var mongoskin = require('mongoskin');
 module.exports = (function(){
     var host = "localhost",
         port = "27017",
-        dbName = "testdb",
+        dbName = "testdb" + process.argv[2],
         str = 'mongodb://'+host+':'+port+'/'+dbName;
     var option={
         native_parser:true
